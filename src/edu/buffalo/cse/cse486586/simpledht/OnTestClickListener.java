c491da0f3138dce8fff3cd8a49d2ad0619af4c3e
Adil <Adil@Roxy>
2013-03-20 18:39:03 -0400

    OnTestClickListener.java as updated by professor himself. He made a mistake :)

--- src/edu/buffalo/cse/cse486586/simpledht/OnTestClickListener.java
+++ src/edu/buffalo/cse/cse486586/simpledht/OnTestClickListener.java
@@ -4,6 +4,7 @@ import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,112 +12,129 @@ import android.widget.TextView;
 
 public class OnTestClickListener implements OnClickListener {
 
-    private static final String TAG = OnTestClickListener.class.getName();
-    private static final int TEST_CNT = 50;
-    private static final String KEY_FIELD = "key";
-    private static final String VALUE_FIELD = "value";
+	private static final String TAG = OnTestClickListener.class.getName();
+	private static final int TEST_CNT = 50;
+	private static final String KEY_FIELD = "key";
+	private static final String VALUE_FIELD = "value";
 
-    private final TextView mTextView;
-    private final ContentResolver mContentResolver;
-    private final Uri mUri;
-    private final ContentValues[] mContentValues;
+	private final TextView mTextView;
+	private final ContentResolver mContentResolver;
+	private final Uri mUri;
+	private final ContentValues[] mContentValues;
 
-    public OnTestClickListener(TextView _tv, ContentResolver _cr) {
-        mTextView = _tv;
-        mContentResolver = _cr;
-        mUri = buildUri("content", "edu.buffalo.cse.cse486586.simpledht.provider");
-        mContentValues = initTestValues();
-    }
+	public OnTestClickListener(TextView _tv, ContentResolver _cr) {
+		mTextView = _tv;
+		mContentResolver = _cr;
+		mUri = buildUri("content", "edu.buffalo.cse.cse486586.simpledht.provider");
+		mContentValues = initTestValues();
+	}
 
-    private Uri buildUri(String scheme, String authority) {
-        Uri.Builder uriBuilder = new Uri.Builder();
-        uriBuilder.authority(authority);
-        uriBuilder.scheme(scheme);
-        return uriBuilder.build();
-    }
+	private Uri buildUri(String scheme, String authority) {
+		Uri.Builder uriBuilder = new Uri.Builder();
+		uriBuilder.authority(authority);
+		uriBuilder.scheme(scheme);
+		return uriBuilder.build();
+	}
 
-    private ContentValues[] initTestValues() {
-        ContentValues[] cv = new ContentValues[TEST_CNT];
-        for (int i = 0; i < TEST_CNT; i++) {
-            cv[i] = new ContentValues();
-            cv[i].put(KEY_FIELD, "key" + Integer.toString(i));
-            cv[i].put(VALUE_FIELD, "val" + Integer.toString(i));
-        }
+	private ContentValues[] initTestValues() {
+		ContentValues[] cv = new ContentValues[TEST_CNT];
+		for (int i = 0; i < TEST_CNT; i++) {
+			cv[i] = new ContentValues();
+			cv[i].put(KEY_FIELD, "key" + Integer.toString(i));
+			cv[i].put(VALUE_FIELD, "val" + Integer.toString(i));
+		}
 
-        return cv;
-    }
+		return cv;
+	}
 
-    @Override
-    public void onClick(View v) {
-        if (testInsert()) {
-            mTextView.append("Insert success\n");
-        } else {
-            mTextView.append("Insert fail\n");
-            return;
-        }
+	@Override
+	public void onClick(View v) {
+		new Task().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+	}
 
-        if (testQuery()) {
-            mTextView.append("Query success\n");
-        } else {
-            mTextView.append("Query fail\n");
-        }
-    }
+	private class Task extends AsyncTask<Void, String, Void> {
 
-    private boolean testInsert() {
-        try {
-            for (int i = 0; i < TEST_CNT; i++) {
-                mContentResolver.insert(mUri, mContentValues[i]);
-            }
-        } catch (Exception e) {
-            Log.e(TAG, e.toString());
-            return false;
-        }
+		@Override
+		protected Void doInBackground(Void... params) {
+			if (testInsert()) {
+				publishProgress("Insert success\n");
+			} else {
+				publishProgress("Insert fail\n");
+				return null;
+			}
 
-        return true;
-    }
+			if (testQuery()) {
+				publishProgress("Query success\n");
+			} else {
+				publishProgress("Query fail\n");
+			}
+			
+			return null;
+		}
+		
+		protected void onProgressUpdate(String...strings) {
+			mTextView.append(strings[0]);
 
-    private boolean testQuery() {
-        try {
-            for (int i = 0; i < TEST_CNT; i++) {
-                String key = (String) mContentValues[i].get(KEY_FIELD);
-                String val = (String) mContentValues[i].get(VALUE_FIELD);
+			return;
+		}
 
-                Cursor resultCursor = mContentResolver.query(mUri, null, key, null, null);
-                if (resultCursor == null) {
-                    Log.e(TAG, "Result null");
-                    throw new Exception();
-                }
+		private boolean testInsert() {
+			try {
+				for (int i = 0; i < TEST_CNT; i++) {
+					mContentResolver.insert(mUri, mContentValues[i]);
+				}
+			} catch (Exception e) {
+				Log.e(TAG, e.toString());
+				return false;
+			}
 
-                int keyIndex = resultCursor.getColumnIndex(KEY_FIELD);
-                int valueIndex = resultCursor.getColumnIndex(VALUE_FIELD);
-                if (keyIndex == -1 || valueIndex == -1) {
-                    Log.e(TAG, "Wrong columns");
-                    resultCursor.close();
-                    throw new Exception();
-                }
+			return true;
+		}
 
-                resultCursor.moveToFirst();
+		private boolean testQuery() {
+			try {
+				for (int i = 0; i < TEST_CNT; i++) {
+					String key = (String) mContentValues[i].get(KEY_FIELD);
+					String val = (String) mContentValues[i].get(VALUE_FIELD);
 
-                if (!(resultCursor.isFirst() && resultCursor.isLast())) {
-                    Log.e(TAG, "Wrong number of rows");
-                    resultCursor.close();
-                    throw new Exception();
-                }
+					Cursor resultCursor = mContentResolver.query(mUri, null,
+							key, null, null);
+					if (resultCursor == null) {
+						Log.e(TAG, "Result null");
+						throw new Exception();
+					}
 
-                String returnKey = resultCursor.getString(keyIndex);
-                String returnValue = resultCursor.getString(valueIndex);
-                if (!(returnKey.equals(key) && returnValue.equals(val))) {
-                    Log.e(TAG, "(key, value) pairs don't match\n");
-                    resultCursor.close();
-                    throw new Exception();
-                }
+					int keyIndex = resultCursor.getColumnIndex(KEY_FIELD);
+					int valueIndex = resultCursor.getColumnIndex(VALUE_FIELD);
+					if (keyIndex == -1 || valueIndex == -1) {
+						Log.e(TAG, "Wrong columns");
+						resultCursor.close();
+						throw new Exception();
+					}
 
-                resultCursor.close();
-            }
-        } catch (Exception e) {
-            return false;
-        }
+					resultCursor.moveToFirst();
 
-        return true;
-    }
+					if (!(resultCursor.isFirst() && resultCursor.isLast())) {
+						Log.e(TAG, "Wrong number of rows");
+						resultCursor.close();
+						throw new Exception();
+					}
+
+					String returnKey = resultCursor.getString(keyIndex);
+					String returnValue = resultCursor.getString(valueIndex);
+					if (!(returnKey.equals(key) && returnValue.equals(val))) {
+						Log.e(TAG, "(key, value) pairs don't match\n");
+						resultCursor.close();
+						throw new Exception();
+					}
+
+					resultCursor.close();
+				}
+			} catch (Exception e) {
+				return false;
+			}
+
+			return true;
+		}
+	}
 }
